9287875b91f3cfa93609dd3c3bde10a7e787e081
JERRYJUQU <jerryjuqu@gmail.com>
2020-08-07 14:46:37 -0400

    Update item.h

--- Code/item.h
+++ Code/item.h
@@ -1,11 +1,23 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#include <vector>
+#include <memory>
 #include "position.h"
+class Observer;
+class Hero;
 
 class Item {
 	Position pos;
+	std::vector <std::shared_ptr <Observer>> observers;
 
+public:
+	Item(int x, int y);
+	virtual void effect(std::shared_ptr<Hero> hero) = 0;
+	Position getPosition();
+	void attach(std::shared_ptr <Observer> observer);
+	void detach(std::shared_ptr <Observer> observer);
+	void notify(std::shared_ptr <Observer> observer);
 };
 
 #endif
