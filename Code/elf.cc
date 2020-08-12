8f2d43cf5a91e7e6538a310b2d308410cc8b73ac
oahznwahs <56942310+oahznwahs@users.noreply.github.com>
2020-08-12 18:50:26 -0400

    Update elf.cc

--- Code/elf.cc
+++ Code/elf.cc
@@ -1,9 +1,10 @@
 #include "elf.h"
 
-Elf::Elf(Position pos): Hero{pos} {
+Elf::Elf(Position pos): Hero{pos, EnemyType::elf} {
     hp = 140;
     atk = 30;
     def = 10;
+    maxHP = 140;
 }
     
 void Elf::defend(Hero & hero){
@@ -11,23 +12,58 @@ void Elf::defend(Hero & hero){
 }
     
 void Elf::attack(Shade & shade){
-    shade.incHP(-2*((100/(100+shade.getDef()))*(this->getAtk())));
+    int miss = std::rand()%2;
+    if(!miss){
+        shade.incHP(-((100/(100+shade.getDef()))*(this->getAtk())));
+    }
+    miss = std::rand()%2;
+    if(!miss){
+        shade.incHP(-((100/(100+shade.getDef()))*(this->getAtk())));
+    }
 }
     
 void Elf::attack(Drow & drow){
-    drow.incHP(-((100/(100+drow.getDef()))*(this->getAtk())));
+    int miss = std::rand()%2;
+    if(!miss){
+        drow.incHP(-((100/(100+drow.getDef()))*(this->getAtk())));
+    }
+    miss = std::rand()%2;
+    if(!miss){
+        drow.incHP(-((100/(100+drow.getDef()))*(this->getAtk())));
+    }
 }
 
 void Elf::attack(Vampire & vampire){
-    vampire.incHP(-2*((100/(100+vampire.getDef()))*(this->getAtk())));
+    int miss = std::rand()%2;
+    if(!miss){
+        vampire.incHP(-((100/(100+vampire.getDef()))*(this->getAtk())));
+    }
+    miss = std::rand()%2;
+    if(!miss){
+        vampire.incHP(-((100/(100+vampire.getDef()))*(this->getAtk())));
+    }
 }
 
 void Elf::attack(Troll & troll){
-    troll.incHP(-2*((100/(100+troll.getDef()))*(this->getAtk())));
+    int miss = std::rand()%2;
+    if(!miss){
+        troll.incHP(-((100/(100+troll.getDef()))*(this->getAtk())));
+    }
+    miss = std::rand()%2;
+    if(!miss){
+        troll.incHP(-((100/(100+troll.getDef()))*(this->getAtk())));
+    }
 }
 
 void Elf::attack(Goblin & goblin){
-    goblin.incHP(-2*((100/(100+goblin.getDef()))*(this->getAtk())));
+    int miss = std::rand()%2;
+    if(!miss){
+        goblin.incHP(-((100/(100+goblin.getDef()))*(this->getAtk())));
+    }
+    miss = std::rand()%2;
+    if(!miss){
+        goblin.incHP(-((100/(100+goblin.getDef()))*(this->getAtk())));
+    }
 }
 
 void Elf::notifyDeath(){
