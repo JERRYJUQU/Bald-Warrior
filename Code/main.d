d6d43fe1470b0f40de01cdbecb61953e60e68e8e
whimSYZ <brandonshi2000@gmail.com>
2020-08-12 22:59:46 +0800

    new

--- Code/main.d
+++ Code/main.d
@@ -1,6 +1,10 @@
 main.o: main.cc floor.h tile.h position.h subject.h textdisplay.h \
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   observer.h
+=======
+  observer.h treasure.h item.h hero.h character.h enemy.h
+>>>>>>> Stashed changes
 =======
   observer.h treasure.h item.h hero.h character.h enemy.h
 >>>>>>> Stashed changes
