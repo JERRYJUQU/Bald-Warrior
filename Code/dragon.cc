b6c2f27a2841f9713dd62679681457b0785125c4
oahznwahs <56942310+oahznwahs@users.noreply.github.com>
2020-08-12 19:08:25 -0400

    Update dragon.cc

--- Code/dragon.cc
+++ Code/dragon.cc
@@ -4,30 +4,46 @@ Dragon::Dragon(Position pos, shared_ptr<DragonHoard> hoard): Enemy{pos}, hoard{h
     hp = 150;
     atk = 20;
     def = 20;
+    maxHP = 150;
 }
 
 void Dragon::defend(Enemy & enemy){
     enemy.attack(*this);
 }
     
-void Dragon::attack(Shade & shade){
-    shade.incHP(-((100/(100+shade.getDef()))*(this->getAtk())));
+void  Dragon::attack(Shade & shade){
+    int miss = std::rand()%2;
+    if(!miss){
+        shade.incHP(-((100/(100+shade.getDef()))*(this->getAtk())));
+    }
 }
     
-void Dragon::attack(Drow & drow){
-    drow.incHP(-((100/(100+drow.getDef()))*(this->getAtk())));
+void  Dragon::attack(Drow & drow){
+    int miss = std::rand()%2;
+    if(!miss){
+        drow.incHP(-((100/(100+drow.getDef()))*(this->getAtk())));
+    }
 }
 
-void Dragon::attack(Vampire & vampire){
-    vampire.incHP(-((100/(100+vampire.getDef()))*(this->getAtk())));
+void  Dragon::attack(Vampire & vampire){
+    int miss = std::rand()%2;
+    if(!miss){
+        vampire.incHP(-((100/(100+vampire.getDef()))*(this->getAtk())));
+    }
 }
 
-void Dragon::attack(Troll & troll){
-    troll.incHP(-((100/(100+troll.getDef()))*(this->getAtk())));
+void  Dragon::attack(Troll & troll){
+    int miss = std::rand()%2;
+    if(!miss){
+        troll.incHP(-((100/(100+troll.getDef()))*(this->getAtk())));
+    }
 }
 
-void Dragon::attack(Goblin & goblin){
-    goblin.incHP(-((100/(100+goblin.getDef()))*(this->getAtk())));
+void  Dragon::attack(Goblin & goblin){
+    int miss = std::rand()%2;
+    if(!miss){
+        goblin.incHP(-((100/(100+goblin.getDef()))*(this->getAtk())));
+    }
 }
 
 void Dragon::notifyDeath(){
@@ -37,9 +53,4 @@ void Dragon::notifyDeath(){
     hoard->notifyDeath();
 }
 
-void Dragon::notifyObservers(){
-        for(auto p:observers){
-                p.notify(*this);
-        }
-        return;
-}
+DragonHoard Dragon::getHoard(){return *hoard;}
