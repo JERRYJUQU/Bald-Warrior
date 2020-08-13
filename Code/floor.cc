e4d4012f53dc445cbbd0e6a4fb2494e8c1ca2ea1
oahznwahs <56942310+oahznwahs@users.noreply.github.com>
2020-08-12 23:50:39 -0400

    Update floor.cc

--- Code/floor.cc
+++ Code/floor.cc
@@ -264,6 +264,21 @@ T Floor::enumRand() {
     return static_cast<T> (rand() % enumSize);
 }
 
+void Floor::moveEnemy(Enemy & enemy, Direction dir){
+    Position oldPos = enemy->getPos();
+    Position newPos = getNewPos(oldPos, dir);
+  //check weather new position is valid
+    if(newPos.x < 0 || newPos.y > 0){
+    }
+    //check for any collision with other object
+    if(!checkCollision(newPos, "enemy")){
+    }
+    enemy->move(dir);
+    tiles[oldPos.y][oldPos.x]->notifyObservers();
+    enemy->notifyObservers();
+    return;
+}
+
 void Floor::turn() {
     for (int i = 0; i < 25; i++) {
         for (int j = 0; j < 79; j++) {
@@ -271,7 +286,7 @@ void Floor::turn() {
                 if (enemies[i][j]->getHP() <= 0) {
                     enemies[i][j]->notifyDeath();
                     switch (enemies[i][j]->getEnemyType()) {
-                    case EnemyType::dwarf :
+                    case EnemyType::dwarf:
                         
                         break;
                     case EnemyType::human:
@@ -285,7 +300,7 @@ void Floor::turn() {
                         break;
                     }
                     enemies[i][j] = nullptr;
-                }
+                }tydf
                 else if (abs(hero->getPos().x - enemies[i][j]->getPos().x) < 2 &&
                     abs(hero->getPos().y - enemies[i][j]->getPos().y) < 2 &&
                     !enemies[i][j]->getNeutral()) {
